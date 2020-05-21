cf9fc403bf2792d88a65ea921a641da5ee4b6560
wb_mayuzhou_cd <199611290034Myz!>
2020-05-21 14:03:22 +0800

    1

--- Formula/rocketmq@4.7.0.rb
+++ Formula/rocketmq@4.7.0.rb
@@ -30,8 +30,11 @@ class RocketmqAT470 < Formula
     (libexec/"bin/mqlaunch").write launch
     (libexec/"bin/runbroker.sh").rmtree
     (libexec/"bin/runbroker.sh").write runbroker
+    (libexec/"bin/runserver.sh").rmtree
+    (libexec/"bin/runserver.sh").write runserver
     chmod 0555, (libexec/"bin/mqlaunch")
     chmod 0555, (libexec/"bin/runbroker.sh")
+    chmod 0555, (libexec/"bin/runserver.sh")
     Dir.foreach(libexec/"bin") do |f|
       next if f == "." || f == ".." || !File.extname(f).empty?
 
@@ -71,15 +74,16 @@ class RocketmqAT470 < Formula
 
   def runbroker; <<~EOS
     #!/bin/sh
+    .${HOME}/.bash_profile
     error_exit ()
     {
         echo "ERROR: $1 !!"
         exit 1
     }
     
-    [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=$JAVA_HOME
-    [ ! -e "$JAVA_HOME/bin/java" ] && error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)!"
-    
+    [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME= "/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/bin/java"
+    [ ! -e "$JAVA_HOME/bin/java" ] && error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)!" 
+
     export JAVA_HOME
     export JAVA="$JAVA_HOME/bin/java"
     export BASE_DIR=$(dirname $0)/..
@@ -144,6 +148,65 @@ class RocketmqAT470 < Formula
   EOS
   end 
 
+  def runserver; <<~EOS
+    #!/bin/sh
+    .${HOME}/.bash_profile
+    [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME= "/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/bin/java"
+    [ ! -e "$JAVA_HOME/bin/java" ] && error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)!" 
+    
+
+    export JAVA_HOME
+    export JAVA="$JAVA_HOME/bin/java"
+    export BASE_DIR=$(dirname $0)/..
+    export CLASSPATH=.:${BASE_DIR}/conf:${CLASSPATH}
+    
+    #===========================================================================================
+    # JVM Configuration
+    #===========================================================================================
+    # The RAMDisk initializing size in MB on Darwin OS for gc-log
+    DIR_SIZE_IN_MB=600
+    
+    choose_gc_log_directory()
+    {
+        case "`uname`" in
+            Darwin)
+                if [ ! -d "/Volumes/RAMDisk" ]; then
+                    # create ram disk on Darwin systems as gc-log directory
+                    DEV=`hdiutil attach -nomount ram://$((2 * 1024 * DIR_SIZE_IN_MB))` > /dev/null
+                    diskutil eraseVolume HFS+ RAMDisk ${DEV} > /dev/null
+                    echo "Create RAMDisk /Volumes/RAMDisk for gc logging on Darwin OS."
+                fi
+                GC_LOG_DIR="/Volumes/RAMDisk"
+            ;;
+            *)
+                # check if /dev/shm exists on other systems
+                if [ -d "/dev/shm" ]; then
+                    GC_LOG_DIR="/dev/shm"
+                else
+                    GC_LOG_DIR=${BASE_DIR}
+                fi
+            ;;
+        esac
+    }
+    
+    choose_gc_log_directory
+    
+    JAVA_OPT="${JAVA_OPT} -server -Xms2g -Xmx2g -Xmn1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m"
+    JAVA_OPT="${JAVA_OPT} -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8  -XX:-UseParNewGC"
+    JAVA_OPT="${JAVA_OPT} -verbose:gc -Xloggc:${GC_LOG_DIR}/rmq_srv_gc_%p_%t.log -XX:+PrintGCDetails"
+    JAVA_OPT="${JAVA_OPT} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=30m"
+    JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
+    JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages"
+    JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${BASE_DIR}/lib"
+    #JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
+    JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
+    JAVA_OPT="${JAVA_OPT} -cp ${CLASSPATH}"
+    
+    $JAVA ${JAVA_OPT} $@
+
+  EOS
+  end
+  
   def caveats
     s = <<~EOS
       Data:    #{var}/rocketmq/commitlog
